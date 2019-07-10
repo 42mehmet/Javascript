9a838a9b89e482f576f41123645fbbaef85c43db
42mehmet <42mhmtgny@gmail.com>
2019-07-10 12:52:46 +0300

    ödev bitmedi

--- js_temelleri/b.js
+++ js_temelleri/b.js
@@ -1,9 +1,21 @@
-'use strict';
+"use strict";
 
 // Asagidaki `sayilar` dizisinin elemanlarindan kac tane cift, kac tane tek eleman vardir?
 
-{
-  const sayilar = [12, 57, 2, 9, 6, 124, 44, 78, 41];
+const sayilar = [12, 57, 2, 9, 6, 124, 44, 78, 41];
 
-  // Kodunuzu bu satirdan itibaren yaziniz
-}
+// Kodunuzu bu satirdan itibaren yaziniz
+
+// const tek = [];
+// const cift = [];
+// for (let i = 0; i < sayilar.length; i++) {
+//   const element = sayilar[i];
+
+//   if (element % 2 > 0) {
+//     tek.push(sayilar[i]);
+//   } else {
+//     cift.push(sayilar[i]);
+//   }
+// }
+// console.log(cift);
+// console.log(tek
--- js_temelleri/c.js
+++ js_temelleri/c.js
@@ -1,9 +1,23 @@
-'use strict';
+"use strict";
 
 // Asagidaki `sifre` dizisinin elemanlarindan kaci `sayi (number)`, kaci `string` cinsindendir, bulunuz.
 
-{
-  const sifre = ['b', '$', 2, 1, 'X', 0, 0, 'a', 'c'];
+const sifre = ["b", "$", 2, 1, "X", 0, 0, "a", "c"];
 
-  // Kodunuzu bu satirdan itibaren yaziniz
-}
+// Kodunuzu bu satirdan itibaren yaziniz
+// const nmr = [];
+// const str = [];
+// // typeof sifre;
+// // console.log(sifre.);
+
+// for (let i = 0; i < sifre.length; i++) {
+//   const sfr = sifre[i];
+
+//   if (typeof sifre[i] == "string") {
+//     str.push(sifre[i]);
+//   } else {
+//     nmr.push(sifre[i]);
+//   }
+// }
+// console.log(nmr);
+// console.log(str);
